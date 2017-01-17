4bab631ae4af924029c4d72fc63c67c73a8db04d
= <=>
2017-01-16 21:34:55 -0500

    fixed client subfolder bug

--- battlecode-scaffold-2017-master/src/examplefuncsplayer/RobotPlayer.java
+++ battlecode-scaffold-2017-master/src/examplefuncsplayer/RobotPlayer.java
@@ -46,12 +46,12 @@ public strictfp class RobotPlayer {
                 Direction dir = randomDirection();
 
                 // Randomly attempt to build a gardener in this direction
-                if (rc.canHireGardener(dir)) {
+                if (rc.canHireGardener(dir) && Math.random() < .01) {
                     rc.hireGardener(dir);
                 }
 
                 // Move randomly
-                //tryMove(randomDirection());
+                tryMove(randomDirection());
 
                 // Broadcast archon's location for other robots on the team to know
                 MapLocation myLocation = rc.getLocation();
